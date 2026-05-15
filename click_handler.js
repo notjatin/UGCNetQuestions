c3d8a5a665e7db7a85022a775a1687171ed24d97
Jatin Pant <notjatinpant@gmail.com>
2026-05-15 14:17:52 +0530

    first commit

---
+++ click_handler.js
@@ -0,0 +1,6 @@
+// Keeps only the given child active in the passed parent
+function handleActiveClickNoToggle(child, parent) {
+    // Throw error if child is not inside parent as the first nested child
+
+    
+}
